dcd94e00e91d388bc198335e6bb9343170c88a64
Lysanthir <79011413+Evelorne@users.noreply.github.com>
2022-08-12 21:00:50 +0300

    commit

--- src/ciftsayi.java
+++ src/ciftsayi.java
@@ -2,14 +2,16 @@ import java.util.Scanner ;
 
 public class ciftsayi {
     public static void main(String[] args) {
+        int z = 0;
         Scanner sc = new Scanner(System.in);
         System.out.print("Sayı giriniz : ");
         int a = sc.nextInt();
-        for (int i = 0; i <= a; i+=2) {
-            System.out.println(i);
-            if(i%2!=0){
-                continue;
+        for (int i = 0; i <= a; i++) {
+            if(i%3==0 && i%4==0){
+
+                z+= i;
             }
         }
+        System.out.println(z);
     }
 }
